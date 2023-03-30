a8d173ca05b3772edadca0f5db43111d0ac1edb5
1CLC1 <ChrisC1L1C@gmail.com>
2023-03-29 21:36:12 -0400

    the color feature has been added

--- src/Main.java
+++ src/Main.java
@@ -15,5 +15,10 @@ public class Main {
         neon.color="red";
         neon.size="15cm";
         neon.intensity="7675lumin";
+
+        Color col= new Color();
+        col.colorLights="True";
+        col.colorTyre="false";
+        col.colorWindows="True";
     }
 }
